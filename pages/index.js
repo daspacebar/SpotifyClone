ac49a63a2adff18e39a99fb411c54734d48a7697
Niket Kumar <niket123kumar@gmail.com>
2021-12-25 16:58:18 +0530

    initial commit/1

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Sidebar from '../components/Sidebar'
+
+export default function Home() {
+  return (
+    <div className="">
+      <main>
+        <Sidebar />
+        {/* center */}
+      </main>
+      <div>{/* player */}</div>
+    </div>
+  )
+}
