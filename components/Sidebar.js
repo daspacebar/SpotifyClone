8fd209d6eacb222371fb0b779d73d59c656dbb13
Niket Kumar <niket123kumar@gmail.com>
2021-12-25 17:45:18 +0530

    Sidebar Complete

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -32,6 +32,28 @@ function Sidebar() {
                     <p>Your Library</p>
                 </button>
                 <hr className='border-t-[0.1px] border-gray-900' />
+
+                <p className='cursor-pointer hover:text-white'>
+                    Playlist name...
+                </p>
+                <p className='cursor-pointer hover:text-white'>
+                    Playlist name...
+                </p>
+                <p className='cursor-pointer hover:text-white'>
+                    Playlist name...
+                </p>
+                <p className='cursor-pointer hover:text-white'>
+                    Playlist name...
+                </p>
+                <p className='cursor-pointer hover:text-white'>
+                    Playlist name...
+                </p>
+                <p className='cursor-pointer hover:text-white'>
+                    Playlist name...
+                </p>
+                <p className='cursor-pointer hover:text-white'>
+                    Playlist name...
+                </p>
             </div>
         </div>
     )
