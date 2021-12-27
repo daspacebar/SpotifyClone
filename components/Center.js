ae62f9b826117093c8108a46489cebec22d0d48c
Niket Kumar <niket123kumar@gmail.com>
2021-12-27 23:17:38 +0530

    NextAuth Complete/test-logout-complete

---
+++ components/Center.js
@@ -0,0 +1,9 @@
+function Center() {
+    return (
+        <div>
+            <h1 className="">I am center</h1>
+        </div>
+    )
+}
+
+export default Center
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Center from '../components/Center'
 import Sidebar from '../components/Sidebar'
 
 export default function Home() {
