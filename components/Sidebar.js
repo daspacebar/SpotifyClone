91accfbee7cc84e55cac7ce093a20fee449cc661
Niket Kumar <niket123kumar@gmail.com>
2022-01-12 23:22:45 +0530

    spotify hooks/playlist fetching

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -5,20 +5,20 @@ import useSpotify from '../hooks/useSpotify';
 
 
 function Sidebar() {
-
     const spotifyApi = useSpotify();
     const {data: session, status} = useSession();
-    const [playlists, setPlaylists] = useState([])
+    const [playlists, setPlaylists] = useState([]);
+    const [playlistId, setPlaylistId] = useState(null);
 
     useEffect(() => {
-        if(spotifyApi.getAccessToken){
+        if(spotifyApi.getAccessToken()){
             spotifyApi.getUserPlaylists().then((data) => {
                 setPlaylists(data.body.items);
             })
         }
     }, [session, spotifyApi])
 
-    console.log(playlists);
+    console.log("you picked the playlist", playlistId);
 
     return (
         <div className='text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll h-screen scrollbar-hide'>
@@ -57,27 +57,13 @@ function Sidebar() {
                 </button>
                 <hr className='border-t-[0.1px] border-gray-900' />
 
-                <p className='cursor-pointer hover:text-white'>
-                    Playlist name...
-                </p>
-                <p className='cursor-pointer hover:text-white'>
-                    Playlist name...
-                </p>
-                <p className='cursor-pointer hover:text-white'>
-                    Playlist name...
-                </p>
-                <p className='cursor-pointer hover:text-white'>
-                    Playlist name...
-                </p>
-                <p className='cursor-pointer hover:text-white'>
-                    Playlist name...
-                </p>
-                <p className='cursor-pointer hover:text-white'>
-                    Playlist name...
-                </p>
-                <p className='cursor-pointer hover:text-white'>
-                    Playlist name...
-                </p>
+                {playlists.map((playlist) => (
+                    <p key={playlist.id} onClick={() => {
+                        setPlaylistId(playlist.id);
+                    }} className='cursor-pointer hover:text-white'>
+                        {playlist.name}
+                    </p>
+                ))}
             </div>
         </div>
     )
--- hooks/useSpotify.js
+++ hooks/useSpotify.js
@@ -1,26 +1,26 @@
-import { useEffect } from "react";
+import { useEffect } from "react"
 import { signIn, useSession } from "next-auth/react"
-// import spotifyApi from "../lib/spotify"
-import SpotifyWebApi from "spotify-web-api-node";
+import SpotifyWebAPI from "spotify-web-api-node"
 
-const spotifyApi = new SpotifyWebApi({
+const spotifyApi = new SpotifyWebAPI({
     clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
     clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
 })
 
+
 function useSpotify() {
     const {data: session, status} = useSession();
-    
+
     useEffect(() => {
         if(session){
-          if(session.user === "RefreshAccessTokenError"){
-              signIn();
-          }  
-          spotifyApi.setAccessToken(session.user.accessToken);
+            if(session.error === 'RefreshAccessTokenError'){
+                signIn();
+            }
+            spotifyApi.setAccessToken(session.user.accessToken);
         }
+
     }, [session])
     
     return spotifyApi;
-}
-
+} 
 export default useSpotify
