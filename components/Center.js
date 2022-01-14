b15c2370de271696079847d67e401fbbd5d455bb
Niket Kumar <niket123kumar@gmail.com>
2022-01-14 16:35:04 +0530

    songs grid and duration

--- components/Center.js
+++ components/Center.js
@@ -1,5 +1,5 @@
 import { ChevronDownIcon } from "@heroicons/react/outline";
-import { useSession } from "next-auth/react"
+import { signOut, useSession } from "next-auth/react"
 import { shuffle } from "lodash"
 import { useEffect, useState } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
@@ -43,7 +43,9 @@ function Center() {
     return (
         <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
             <header className="absolute top-5 right-8">
-                <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white">
+                <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white" 
+                    onClick={signOut}
+                >
                     <img className="rounded-full w-10 h-10" src={session?.user.image} />
                     <h1>{session?.user.name}</h1>
                     <ChevronDownIcon className="h-5 2-5" />
